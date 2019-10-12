a719399eda9312783450f6122aa2ab99b10430b2
yuslianto <yuslianto56@gmail.com>
2019-10-12 21:43:20 +0700

    changes resume

---
+++ precache-manifest.afb1a47c72d02cf88f199a6fa4080196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3778150b5c1d0015ca5481927e85a7e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55d9eb0f3dd9e04d6cdf",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "df78767951b7c7096d35",
+    "url": "/static/js/2.c23a251d.chunk.js"
+  },
+  {
+    "revision": "55d9eb0f3dd9e04d6cdf",
+    "url": "/static/js/main.62acd2ac.chunk.js"
+  },
+  {
+    "revision": "71d00d19d79552364d1c",
+    "url": "/static/js/runtime-main.5591b063.js"
+  }
+]);
